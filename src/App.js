47bce2868f31ae13e5e9582e55c6806f7d9b25c8
AishwaryaSB99 <aishwaryasampathsb@gmail.com>
2024-12-15 19:27:33 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import CatNav from "./components/CatNav";
+import MainComponent from "./components/MainComponent";
+import TopNav from "./components/TopNav";
+
+function App() {
+  return (
+    <div className="App">
+      <TopNav />
+      <CatNav />
+      <MainComponent />
+    </div>
+  );
+}
+
+export default App;
